076cae4bf63e1f351cb3932317fdfe6a032d3bc4
cabrinilm <luca_cabrinimm@hotmail.com>
2025-11-03 18:55:53 +0000

    get-jwt.js updated

--- backend/src/services/supabaseClient.ts
+++ backend/src/services/supabaseClient.ts
@@ -6,4 +6,8 @@ dotenv.config();
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Supabase URL and anon key must be defined in .env');
+  }
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
