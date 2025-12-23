27d7fa5ec6bded889c00bfcce0c4c8a00a6ffbf2
cabrinilm <luca_cabrinimm@hotmail.com>
2025-12-23 12:14:16 +0000

    profile.service.test test add

--- backend/__tests__/unit/profile.service.test.ts
+++ backend/__tests__/unit/profile.service.test.ts
@@ -3,7 +3,7 @@ import { getProfile } from "../../src/services/profile.service";
 describe("Profile Service", () => {
   it("should return the user's profile when it exists", async () => {
     const fakeUserId = "123e4567-e89b-12d3-a456-426614174000";
-
+  
     const fakeProfile = {
       id: fakeUserId,
       email: "test@example.com",
@@ -11,35 +11,36 @@ describe("Profile Service", () => {
       created_at: new Date().toISOString(),
       updated_at: new Date().toISOString(),
     };
-
-   
+  
     const mockSupabase = {
       from: () => ({
         select: () => ({
-          eq: () => ({
+          eq: (column: string, value: string) => ({ 
             single: async () => {
-              return { data: fakeProfile, error: null };
+              if (value === fakeUserId) {
+                return { data: fakeProfile, error: null };
+              } else {
+                return { data: null, error: null };
+              }
             },
           }),
         }),
       }),
     };
-
-
+  
     const profile = await getProfile(mockSupabase as any, fakeUserId);
-
-
+  
     expect(profile).toEqual(fakeProfile);
     expect(profile.email).toBe("test@example.com");
     expect(profile.name).toBe("Test User");
   });
-  it("should thrown an error if the profile is not find", async () => {
+  it("should thrown an error if the profile is not found", async () => {
     const fakeUserId = "123e4567-e89b-12d3-a456-426614174000";
     const userNotFound =  "123e4567-e89b-12d3-a456-426614174111"
    
     const fakeProfile = {
       id: fakeUserId,
-      email: "test@example.com",
+      email: "test@example123.com",
       name: "Test User",
       created_at: new Date().toISOString(),
       updated_at: new Date().toISOString(),
@@ -49,9 +50,13 @@ describe("Profile Service", () => {
     const mockSupabase = {
       from: () => ({
         select: () => ({
-          eq: () => ({
+          eq: (column: string, value: string) => ({
             single: async () => {
-              return { data: fakeProfile, error: null };
+             if(value === fakeUserId){
+              return {data: fakeProfile, error:null};
+             } else {
+              return {data: null, error: null};
+             }
             },
           }),
         }),
@@ -59,8 +64,8 @@ describe("Profile Service", () => {
     };
    
     const profile = await getProfile(mockSupabase as any, userNotFound);
-
-    expect(profile).toBe("User not found")
+   
+    expect(profile).toBeNull();
 
 
 
