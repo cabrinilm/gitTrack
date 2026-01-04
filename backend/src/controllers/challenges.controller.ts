c0eb06eabb6fdc65def1c7f002d3f3e52779e50c
cabrinilm <luca_cabrinimm@hotmail.com>
2026-01-04 19:32:03 +0000

    challenge(controller): basic controller created

--- backend/src/controllers/challenges.controller.ts
+++ backend/src/controllers/challenges.controller.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import { getChallenges } from "../services/challenges.service";
+import { createChallenge, getChallenges } from "../services/challenges.service";
 
 export const getMyChallenges = async (
   req: Request,
@@ -26,3 +26,41 @@ export const getMyChallenges = async (
     res.status(500).json({ error: "Failed to fetch challenges" });
   }
 };
+
+
+export const createMyChallenges = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const supabase = req.supabase;
+    const userId = req.user?.id;
+    const {name, description}= req.body;
+
+
+    if (!supabase) {
+      res.status(500).json({ error: "Supabase client not found in request" });
+      return;
+    }
+
+    if (!userId) {
+      res.status(401).json({ error: "Unauthorized: No user ID found" });
+      return;
+    }
+ 
+   
+    if (!name) {
+      res.status(400).json({ error: "Name is required" });
+      return;
+    }
+
+    const newChallenge = await createChallenge(supabase, userId, {name, description})
+
+    res.status(201).json(newChallenge)
+   
+  } catch (error){
+    res.status(500).json({error:"Failed to create new challenge"})
+  };
+
+  
+}
