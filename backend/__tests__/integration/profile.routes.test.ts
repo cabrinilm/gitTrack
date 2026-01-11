26fd169d420900bea0343077a1b0f9467488a56b
cabrinilm <luca_cabrinimm@hotmail.com>
2026-01-11 17:29:09 +0000

    activities.get(unit): all tests passed

--- backend/__tests__/integration/profile.routes.test.ts
+++ backend/__tests__/integration/profile.routes.test.ts
@@ -6,11 +6,7 @@ import { supabase } from "../../src/services/supabaseClient";
 jest.mock("../../src/services/profile.service");
 jest.mock("../../src/services/supabaseClient");
 
-
 describe("Profile", () => {
-
-
-
   describe("GET /api/profile", () => {
     it("should return 200 and the user's profile when authenticated", async () => {
       const fakeProfile = {
@@ -20,19 +16,19 @@ describe("Profile", () => {
         created_at: "2025-12-20T00:00:00Z",
         updated_at: "2025-12-20T00:00:00Z",
       };
-  
+
       (getProfile as jest.Mock).mockResolvedValue(fakeProfile);
-  
+
       (supabase.auth.getUser as jest.Mock).mockResolvedValue({
         data: { user: { id: "123e4567-e89b-12d3-a456-426614174000" } },
         error: null,
       });
-  
+
       const response = await request(app)
         .get("/api/profile")
         .set("Authorization", "Bearer any-fake-token")
         .expect(200);
-  
+
       expect(response.body).toEqual({
         id: fakeProfile.id,
         email: fakeProfile.email,
@@ -43,30 +39,30 @@ describe("Profile", () => {
     });
     it("should return 401 if no token is provided", async () => {
       const response = await request(app).get("/api/profile").expect(401);
-  
+
       expect(response.body.error).toEqual("No token provided");
     });
     it("should return 404 if profile not found", async () => {
       (getProfile as jest.Mock).mockResolvedValue(null);
-  
+
       (supabase.auth.getUser as jest.Mock).mockResolvedValue({
         data: { user: { id: "123..." } },
         error: null,
       });
-  
+
       const response = await request(app)
         .get("/api/profile")
         .set("Authorization", "Bearer fake-token")
         .expect(404);
-  
+
       expect(response.body.error).toBe("Profile not found");
     });
   });
-  
+
   describe("PATCH /api/profile", () => {
     it("should return 200 and the profile updated", async () => {
       const fakeUserId = "123e4567-e89b-12d3-a456-426614174000";
-  
+
       const fakeProfile = {
         id: fakeUserId,
         email: "test@example.com",
@@ -74,105 +70,102 @@ describe("Profile", () => {
         created_at: "2025-12-20T00:00:00Z",
         updated_at: "2025-12-20T00:00:00Z",
       };
-  
+
       const expectedProfile = {
         ...fakeProfile,
         name: "New Name",
         updated_at: "2025-12-21T10:00:00Z",
       };
-  
+
       const update = {
         name: "New Name",
       };
-  
+
       (updateProfile as jest.Mock).mockResolvedValue(expectedProfile);
-  
+
       (supabase.auth.getUser as jest.Mock).mockResolvedValue({
         data: { user: { id: fakeUserId } },
         error: null,
       });
-  
+
       const response = await request(app)
         .patch("/api/profile")
         .set("Authorization", "Bearer any-fake-token")
         .send(update)
         .expect(200);
-  
+
       expect(response.body).toEqual(expectedProfile);
     });
     it("should return 401 if no token is provided", async () => {
       const response = await request(app).post("/api/profile").expect(401);
-  
+
       expect(response.body.error).toEqual("No token provided");
     });
     it("should return 404 if profile does not exist", async () => {
       const fakeUserId = "123e4567-e89b-12d3-a456-426614174000";
-  
+
       const updates = {
         name: "New Name",
       };
-  
+
       (updateProfile as jest.Mock).mockRejectedValue(
         new Error("Profile not found")
       );
-  
+
       (supabase.auth.getUser as jest.Mock).mockResolvedValue({
         data: { user: { id: fakeUserId } },
         error: null,
       });
-  
+
       const response = await request(app)
         .patch("/api/profile")
         .set("Authorization", "Bearer fake-token")
         .send(updates)
         .expect(404);
-  
+
       expect(response.body.error).toBe("Profile not found");
     });
     it("should return 400 if name is invalid (too long)", async () => {
       const invalidUpdate = {
         name: "A".repeat(51),
       };
-  
+
       (supabase.auth.getUser as jest.Mock).mockResolvedValue({
         data: { user: { id: "123..." } },
         error: null,
       });
-  
+
       const response = await request(app)
         .patch("/api/profile")
         .set("Authorization", "Bearer fake-token")
         .send(invalidUpdate)
         .expect(400);
-  
+
       expect(response.body.error).toBe("Invalid name");
     });
     it("should return 500 if an unexpected server error occurs", async () => {
       const fakeUserId = "123e4567-e89b-12d3-a456-426614174000";
-  
-  
+
       const update = {
         name: "New Name",
       };
-  
+
       (updateProfile as jest.Mock).mockRejectedValue(
         new Error("Failed to update profile")
       );
-  
-  
+
       (supabase.auth.getUser as jest.Mock).mockResolvedValue({
         data: { user: { id: fakeUserId } },
         error: null,
       });
-  
+
       const response = await request(app)
         .patch("/api/profile")
         .set("Authorization", "Bearer any-fake-token")
         .send(update)
         .expect(500);
-  
-        expect(response.body.error).toBe("Failed to update profile");
+
+      expect(response.body.error).toBe("Failed to update profile");
     });
   });
-
 });
--- backend/__tests__/unit/activities.service.test.ts
+++ backend/__tests__/unit/activities.service.test.ts
@@ -5,7 +5,7 @@ describe("Activities", () => {
   const fakeUserId = "123e4567-e89b-12d3-a456-426614174000";
   const fakeChallengeId = 2;
 
-  describe("GET /api/activities", () => {
+  describe("GET /api/:challengeId/activities", () => {
     it("returns activities for a valid request", async () => {
       const listActivities = [
         {
@@ -57,6 +57,53 @@ describe("Activities", () => {
       );
 
       expect(userActivities).toEqual(listActivities);
+    });
+    it("returns an empty array when the user has no activities", async () => {
+      const mockSupabase = {
+        from: () => ({
+          select: () => ({
+            eq: (column: string, value: any) => ({
+              eq: (column2: string, value2: any) => ({
+                data:
+                  column === "user_id" &&
+                  value === fakeUserId &&
+                  column2 === "challenge_id" &&
+                  value2 === fakeChallengeId
+                    ? []
+                    : [],
+                error: null,
+              }),
+            }),
+          }),
+        }),
+      };
+
+      const userActivities: Activities[] = await getActivities(
+        mockSupabase as any,
+        fakeUserId,
+        fakeChallengeId
+      );
+
+      expect(userActivities).toEqual([]);
+      expect(userActivities).toHaveLength(0);
+    });
+    it("should throw an error if Supabase returns an error", async () => {
+      const mockSupabase = {
+        from: () => ({
+          select: () => ({
+            eq: (column: string, value: any) => ({
+              eq: (column2: string, value2: any) => ({
+                data: null,
+                error: { message: "Failed to fetch activities" },
+              }),
+            }),
+          }),
+        }),
+      };
+
+      await expect(
+        getActivities(mockSupabase as any, fakeUserId, fakeChallengeId)
+      ).rejects.toThrow("Failed to fetch activities");
     });
   });
 });
