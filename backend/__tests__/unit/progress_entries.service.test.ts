63a621090f4f5e0aaa640f103c66cc564195d5ef
cabrinilm <luca_cabrinimm@hotmail.com>
2026-01-16 20:44:42 +0000

    progress.entries(unit & integration): files add

---
+++ backend/__tests__/unit/progress_entries.service.test.ts
@@ -0,0 +1,9 @@
+
+
+describe("Progress_entries", () => {
+    describe("GET /api/progress", () => {
+        it("returns the progress entries for a valid request", async () => {
+            
+        })
+    })
+})
