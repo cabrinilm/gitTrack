40513ac94eef76dde6f7e995fc510a75be34b441
cabrinilm <luca_cabrinimm@hotmail.com>
2026-01-24 18:07:34 +0000

    first test

--- backend/__tests__/e2e/activities.test.ts
+++ backend/__tests__/e2e/activities.test.ts
@@ -9,3 +9,54 @@ dotenv.config();
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_ANON_KEY!;
 const bearerToken = process.env.SUPABASE_BEARER_TOKEN;
+
+
+describe("Activities", () => {
+
+    let supabase: SupabaseClient<Database>;
+    let userId: string;
+    const testTitlePrefix = "test_event_";
+  
+    const authHeader = { Authorization: `Bearer ${bearerToken}` };
+  
+    const makeRequest = (
+      method: "post" | "get" | "patch" | "delete",
+      url: string,
+      body?: object,
+      headers: { [key: string]: string } = authHeader
+    ) => {
+      let req = request(app)[method](url).set("Content-Type", "application/json");
+      if (headers) req = req.set(headers);
+      if (body) req = req.send(body);
+      return req;
+    };
+  
+    beforeAll(async () => {
+      if (!bearerToken) throw new Error("Missing SUPABASE_BEARER_TOKEN");
+  
+      supabase = createClient<Database>(supabaseUrl, supabaseKey, {
+        global: { headers: authHeader },
+      });
+  
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+  
+      if (!user) throw new Error("Test user not found");
+      userId = user.id;
+  
+   
+    });
+  
+    afterEach(async () => {
+      if (!userId) return;
+  
+      await supabase
+        .from("activities")
+        .delete()
+        .eq("creator_id", userId)
+        .like("title", `${testTitlePrefix}%`);
+    });
+
+
+})
