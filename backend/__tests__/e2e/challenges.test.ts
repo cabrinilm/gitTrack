ab8ef0d2d39964488c3e272ebe982c8abb857265
cabrinilm <luca_cabrinimm@hotmail.com>
2026-01-27 18:34:58 +0000

    challenges.test(e2e): second user add

--- backend/__tests__/e2e/challenges.test.ts
+++ backend/__tests__/e2e/challenges.test.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
-import  app from "../../src/server";
+import app from "../../src/server";
 import dotenv from "dotenv";
 import { Database } from "../../src/types/supabase";
 // import { Challenges } from "../../src/services/challenges.service";
@@ -10,81 +10,100 @@ dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_ANON_KEY!;
-const bearerToken = process.env.SUPABASE_BEARER_TOKEN;
-
+const bearerTokenUserA = process.env.SUPABASE_BEARER_TOKEN!;
+const bearerTokenUserB = process.env.SUPABASE_BEARER_TOKEN_2USER!;
 
 describe("Challenge", () => {
+  let supabaseUserA: SupabaseClient<Database>;
+  let supabaseUserB: SupabaseClient<Database>;
 
-    let supabase: SupabaseClient<Database>;
-    let userId: string;
-    const testNamePrefix = "Test Challenge";
+  let userAId: string;
+  let userBId: string;
+  const testNamePrefix = "Test Challenge";
 
-  
-    const authHeader = { Authorization: `Bearer ${bearerToken}` };
-  
-    const makeRequest = (
-      method: "post" | "get" | "patch" | "delete",
-      url: string,
-      body?: object,
-      headers: { [key: string]: string } = authHeader
-    ) => {
-      let req = request(app)[method](url).set("Content-Type", "application/json");
-      if (headers) req = req.set(headers);
-      if (body) req = req.send(body);
-      return req;
-    };
-  
-    beforeAll(async () => {
-      if (!bearerToken) throw new Error("Missing SUPABASE_BEARER_TOKEN");
+ 
+const authHeaderUserA = { Authorization: `Bearer ${bearerTokenUserA}` };
+const authHeaderUserB = { Authorization: `Bearer ${bearerTokenUserB}` };
 
-    supabase = createClient<Database>(supabaseUrl, supabaseKey, {
-      global: { headers: authHeader },
+  const makeRequest = (
+    method: "post" | "get" | "patch" | "delete",
+    url: string,
+    body?: object,
+    headers?: { [key: string]: string } 
+  ) => {
+    let req = request(app)[method](url).set("Content-Type", "application/json");
+    if (headers) req = req.set(headers);
+    if (body) req = req.send(body);
+    return req;
+  };
+
+  beforeAll(async () => {
+    if (!bearerTokenUserA || !bearerTokenUserB) {
+      throw new Error("Missing bearer tokens");
+    }
+  
+    supabaseUserA = createClient(supabaseUrl, supabaseKey, {
+      global: { headers: authHeaderUserA },
     });
-
-    const { data: { user }, error } = await supabase.auth.getUser();
-
-    if (error || !user) throw new Error("Test user not found or error: " + error?.message);
-    userId = user.id;
+  
+    supabaseUserB = createClient(supabaseUrl, supabaseKey, {
+      global: { headers: authHeaderUserB },
     });
   
-    afterEach(async () => {
-      if (!userId) return;
+    const { data: userAData } = await supabaseUserA.auth.getUser();
+    const { data: userBData } = await supabaseUserB.auth.getUser();
   
-      await supabase
+    if (!userAData.user || !userBData.user) {
+      throw new Error("Users not found");
+    }
+  
+    userAId = userAData.user.id;
+    userBId = userBData.user.id;
+  });
+
+  afterEach(async () => {
+  
+      await supabaseUserA
         .from("challenges")
         .delete()
-        .eq("user_id", userId)
+        .eq("user_id", userAId)
         .ilike("name", `${testNamePrefix}%`);
-    });
-  describe("POST /api/challenges",  () => {
+    
+      await supabaseUserB
+        .from("active_challenges")
+        .delete()
+        .eq("user_id", userBId);
+
+  });
+  describe("POST /api/challenges", () => {
     it("should create challenge and activate it", async () => {
       const body: CreateChallengeInput = {
-        name: `${testNamePrefix} - ${Date.now()}`, 
+        name: `${testNamePrefix} - ${Date.now()}`,
         description: "New Challenge",
       };
 
-      const res = await makeRequest("post", "/api/challenges", body).expect(201);
+      const res = await makeRequest("post", "/api/challenges", body, authHeaderUserA).expect(
+        201
+      );
 
       expect(res.body).toMatchObject({
         name: body.name,
         description: body.description,
-        user_id: userId,
+        user_id: userAId,
       });
       expect(res.body.id).toBeDefined();
       expect(res.body.created_at).toBeDefined();
-    
-   
 
-  
-      const activateChallenge = await makeRequest("post", `/api/challenges/${res.body.id}/activate`).expect(201)
-    
+      const activateChallenge = await makeRequest(
+        "post",
+        `/api/challenges/${res.body.id}/activate`, undefined ,authHeaderUserA
+      ).expect(201);
+
       expect(activateChallenge.body).toMatchObject({
-        user_id: userId,
+        user_id: userAId,
         challenge_id: res.body.id,
-        activated_at: expect.any(String)
-      })
-
+        activated_at: expect.any(String),
+      });
     });
   });
-
 });
