83ff4342a7e0bb79a379d63e4387e6d5cca81d54
cabrinilm <luca_cabrinimm@hotmail.com>
2026-01-28 20:22:36 +0000

    happy path created

---
+++ backend/__tests__/e2e/fulfillments.test.ts
@@ -0,0 +1,123 @@
+import request from "supertest";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import app from "../../src/server";
+import dotenv from "dotenv";
+import { Database } from "../../src/types/supabase";
+import { CreateChallengeInput } from "../../src/types/challenges.types";
+import { CreateActivitiesInput } from "../../src/types/activities.types";
+
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_ANON_KEY!;
+const bearerTokenUserA = process.env.SUPABASE_BEARER_TOKEN!;
+const bearerTokenUserB = process.env.SUPABASE_BEARER_TOKEN_2USER!;
+
+describe("Fulfillments", () => {
+  let supabaseUserA: SupabaseClient<Database>;
+  let supabaseUserB: SupabaseClient<Database>;
+
+
+  let userAId: string;
+  let userBId: string;
+  let challengeId: number;
+
+   let activityId1: number;
+   let activityId2: number;  
+   let activityId3: number;  
+
+  const authHeaderUserA = { Authorization: `Bearer ${bearerTokenUserA}` };
+  const authHeaderUserB = { Authorization: `Bearer ${bearerTokenUserB}` };
+
+
+
+  const makeRequest = (
+    method: "post" | "get" | "patch" | "delete",
+    url: string,
+    body?: object,
+    headers?: { [key: string]: string }
+  ) => {
+    let req = request(app)[method](url).set("Content-Type", "application/json");
+    if (headers) req = req.set(headers);
+    if (body) req = req.send(body);
+    return req;
+  };
+
+  beforeAll(async () => {
+      if (!bearerTokenUserA || !bearerTokenUserB) {
+        throw new Error("Missing bearer tokens");
+      }
+  
+      supabaseUserA = createClient(supabaseUrl, supabaseKey, {
+        global: { headers: authHeaderUserA },
+      });
+  
+      supabaseUserB = createClient(supabaseUrl, supabaseKey, {
+        global: { headers: authHeaderUserB },
+      });
+  
+      const { data: userAData } = await supabaseUserA.auth.getUser();
+      const { data: userBData } = await supabaseUserB.auth.getUser();
+    
+  
+      if (!userAData.user || !userBData.user) {
+        throw new Error("Users not found");
+      }
+  
+      userAId = userAData.user.id;
+      userBId = userBData.user.id;
+      
+      const challengeBody: CreateChallengeInput = {
+        name: `Test Challenge ${Date.now()}`,
+        description: "Test",
+      };
+      const challengeRes = await makeRequest(
+        "post",
+        "/api/challenges",
+        challengeBody,
+        authHeaderUserA
+      );
+      challengeId = challengeRes.body.id;
+
+
+      const newActivity: CreateActivitiesInput ={
+        name: "Gym workout",
+        duration_minutes: 60,
+      }
+
+      const newActivity2: CreateActivitiesInput ={
+        name: "Gym workout",
+        duration_minutes: 60,
+      }
+
+      const newActivity3: CreateActivitiesInput ={
+        name: "Gym workout",
+        duration_minutes: 60,
+      }
+
+      const activityRes1 = await makeRequest("post", `/api/challenges/${challengeId}/activities`, newActivity, authHeaderUserA);
+      const activityRes2= await makeRequest("post", `/api/challenges/${challengeId}/activities`, newActivity2, authHeaderUserA);
+      const activityRes3= await makeRequest("post", `/api/challenges/${challengeId}/activities`, newActivity3, authHeaderUserA);
+    
+      activityId1 = activityRes1.body.id
+      activityId2 = activityRes2.body.id
+      activityId3 = activityRes3.body.id
+    
+    
+    });
+    afterEach(async () => {
+      await supabaseUserA
+        .from("activities")
+        .delete()
+        .eq("challenge_id", challengeId);
+      await supabaseUserA.from("challenges").delete().eq("id", challengeId);
+    });
+    describe("POST /api/progress/fulfillments", () => {
+        it("should fulfill the marked activities",  async () => {
+
+            const res = await makeRequest("post", "/api/progress/fulfillments", {activityId1}, authHeaderUserA ).expect(201)
+
+        });
+    });
+
+});
