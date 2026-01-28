928d45983fc7fe66d99290e4c28c9743c64c9074
cabrinilm <luca_cabrinimm@hotmail.com>
2026-01-28 19:00:55 +0000

    activities.post(e2e

--- backend/__tests__/e2e/activities.test.ts
+++ backend/__tests__/e2e/activities.test.ts
@@ -3,6 +3,7 @@ import { createClient, SupabaseClient } from "@supabase/supabase-js";
 import app from "../../src/server";
 import dotenv from "dotenv";
 import { Database } from "../../src/types/supabase";
+import { CreateActivitiesInput } from "../../src/types/activities.types";
 
 dotenv.config();
 
@@ -50,6 +51,8 @@ describe("Activities", () => {
 
     const { data: userAData } = await supabaseUserA.auth.getUser();
     const { data: userBData } = await supabaseUserB.auth.getUser();
+    console.log("DB user:", userAData);
+    console.log("DB user:", userBData);
 
     if (!userAData.user || !userBData.user) {
       throw new Error("Users not found");
@@ -68,28 +71,44 @@ describe("Activities", () => {
       authHeaderUserA
     );
     challengeId = challengeRes.body.id;
-  
-
+  });
   afterEach(async () => {
-    await supabaseUserA.from("activities").delete().eq("challenge_id", challengeId);
-
-
+    // await supabaseUserA
+    //   .from("activities")
+    //   .delete()
+    //   .eq("challenge_id", challengeId);
+    // await supabaseUserA.from("challenges").delete().eq("id", challengeId);
   });
 
-  afterAll(async () => {
+  describe("POST /api/challenges/:challengeId/activities", () => {
+    const body: CreateActivitiesInput = {
+      name: "Gym workout",
+      duration_minutes: 60,
+    };
 
-    await supabaseUserA.from("challenges").delete().eq("id", challengeId);
-  });
-  
-});
-describe("POST /api/challenges/:challengeId/activities" , () => {
     it("should create activities in the specified challengeId", async () => {
+      const res = await makeRequest(
+        "post",
+        `/api/challenges/${challengeId}/activities`,
+        body,
+        authHeaderUserA
+      ).expect(201);
 
-
-
-
-        
-
+      expect(res.body).toMatchObject({
+        id: res.body.id,
+        challenge_id: challengeId,
+        name: body.name,
+        duration_minutes: body.duration_minutes,
+        order_num: res.body.order_num,
+      });
     });
-});
+    it.only("should not allow user create activity with another users challengeId", async () => {
+      const res = await makeRequest(
+        "post",
+        `/api/challenges/${challengeId}/activities`,
+        body,
+        authHeaderUserB
+      ).expect(403);
+    });
+  });
 });
